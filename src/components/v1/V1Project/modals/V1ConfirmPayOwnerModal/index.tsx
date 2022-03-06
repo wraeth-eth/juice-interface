36ab1e83496d921ed06a240a91d7bcbc1685df85
Johnny D <johnnyd.eth@protonmail.com>
2022-03-06 21:40:09 +1000

    implement currencyContext and make version agnostic payInput component

--- src/components/v1/V1Project/modals/V1ConfirmPayOwnerModal/index.tsx
+++ src/components/v1/V1Project/modals/V1ConfirmPayOwnerModal/index.tsx
@@ -8,17 +8,19 @@ import { NetworkContext } from 'contexts/networkContext'
 import { V1ProjectContext } from 'contexts/v1/projectContext'
 import * as constants from '@ethersproject/constants'
 import { useCurrencyConverter } from 'hooks/v1/CurrencyConverter'
-import { usePayProjectTx } from 'hooks/v1/transactor/PayProjectTx'
+
 import { useContext, useState } from 'react'
 import { V1CurrencyName } from 'utils/v1/currency'
 import { formattedNum, formatWad } from 'utils/formatNumber'
 import { weightedRate } from 'utils/math'
 import { tokenSymbolText } from 'utils/tokenSymbolText'
+import { decodeFundingCycleMetadata } from 'utils/v1/fundingCycle'
+import { usePayV1ProjectTx } from 'hooks/v1/transactor/PayV1ProjectTx'
 
-import ProjectRiskNotice from './ProjectRiskNotice'
+import V1ProjectRiskNotice from './V1ProjectRiskNotice'
 import { V1_CURRENCY_ETH, V1_CURRENCY_USD } from 'constants/v1/currency'
 
-export default function ConfirmPayOwnerModal({
+export default function V1ConfirmPayOwnerModal({
   visible,
   weiAmount,
   onSuccess,
@@ -32,22 +34,23 @@ export default function ConfirmPayOwnerModal({
   const [loading, setLoading] = useState<boolean>()
   const [preferUnstaked, setPreferUnstaked] = useState<boolean>(false)
   const [form] = useForm<{ note: string }>()
-  const { userAddress } = useContext(NetworkContext)
+  const { userAddress, onSelectWallet } = useContext(NetworkContext)
   const { tokenSymbol, tokenAddress, currentFC, metadata } =
     useContext(V1ProjectContext)
-  const payProjectTx = usePayProjectTx()
-
   const converter = useCurrencyConverter()
 
   const usdAmount = converter.weiToUsd(weiAmount)
 
+  const payProjectTx = usePayV1ProjectTx()
+
   async function pay() {
     if (!weiAmount) return
-
     await form.validateFields()
 
     if (userAddress) {
       setLoading(true)
+    } else {
+      if (onSelectWallet) onSelectWallet()
     }
 
     payProjectTx(
@@ -65,8 +68,22 @@ export default function ConfirmPayOwnerModal({
     )
   }
 
-  const receivedTickets = weightedRate(currentFC, weiAmount, 'payer')
-  const ownerTickets = weightedRate(currentFC, weiAmount, 'reserved')
+  const fcReservedRate = decodeFundingCycleMetadata(
+    currentFC?.metadata,
+  )?.reservedRate
+
+  const receivedTickets = weightedRate(
+    currentFC?.weight,
+    fcReservedRate,
+    weiAmount,
+    'payer',
+  )
+  const ownerTickets = weightedRate(
+    currentFC?.weight,
+    fcReservedRate,
+    weiAmount,
+    'reserved',
+  )
 
   const hasIssuedTokens = tokenAddress && tokenAddress !== constants.AddressZero
 
@@ -86,7 +103,7 @@ export default function ConfirmPayOwnerModal({
       <Space direction="vertical" size="large" style={{ width: '100%' }}>
         <p>
           <Trans>
-            Paying {metadata.name} is not an investment—it's a way to support
+            Paying {metadata.name} is not an investment — it's a way to support
             the project. Any value or utility of the tokens you receive is
             determined by {metadata.name}.
           </Trans>
@@ -101,7 +118,7 @@ export default function ConfirmPayOwnerModal({
           </div>
         )}
 
-        <ProjectRiskNotice />
+        <V1ProjectRiskNotice />
 
         <Descriptions column={1} bordered>
           <Descriptions.Item label={t`Pay amount`} className="content-right">
